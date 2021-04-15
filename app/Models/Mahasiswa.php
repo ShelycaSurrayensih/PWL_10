962078b88095f696350726067a6dfecfb8a8a3d0
Shelyca Surrayensih <70507995+ShelycaSurrayensih@users.noreply.github.com>
2021-04-15 16:17:37 +0700

    "Tugas - mendefinisikan relasi pada model Mahasiswa"

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;  //Model Eloquent
+// use App\Models\Mahasiswa;
+
+class Mahasiswa extends Model //Definisi Model
+{
+    protected $table="mahasiswa"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
+    protected $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
+    /**
+     * The attributes that are mass assignable. *
+     * @var array
+     */
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas_id',
+        'jurusan',
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class);
+    }
+}
